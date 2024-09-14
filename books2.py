a7d84352a761083cbed5b9709eedc1e310889f5b
Igor Dias <igordiasth@gmail.com>
2024-09-14 11:21:08 -0400

    add validation BookRequest `books2.py`

--- books2.py
+++ books2.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Body
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,8 +21,16 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+
+
 BOOKS = [
-    Book(1, 'Python', 'Likry', 'Very nice book', 5),
+    Book(1, 'Python', 'Jack Jan', 'Very nice book', 5),
     Book(2, 'JavaScript', 'John Doe', 'Good book', 4),
     Book(3, 'Go', 'Jane Doe', 'Great book', 2),
     Book(4, 'Rust', 'David Johnson', 'Fantastic book', 4),
@@ -34,5 +45,16 @@ async def get_all_books():
 
 
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):  # book_request is a type of BookRequest
+    new_book = Book(**book_request.model_dump())  # converting the request to Book object
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+
+    return book
