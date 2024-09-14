e5d87799075deba0131d94691b2c11604cefda50
Igor Dias <igordiasth@gmail.com>
2024-09-14 10:44:12 -0400

    add class and list of books `books2.py`

---
+++ books2.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+BOOKS = [
+    Book(1, 'Python', 'Likry', 'Very nice book', 5),
+    Book(2, 'JavaScript', 'John Doe', 'Good book', 4),
+    Book(3, 'Go', 'Jane Doe', 'Great book', 2),
+    Book(4, 'Rust', 'David Johnson', 'Fantastic book', 4),
+    Book(5, 'C#', 'Mary Smith', 'Excellent book', 3),
+    Book(6, 'Java', 'John Williams', 'Average book', 3)
+]
+
+
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
